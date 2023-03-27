ece2ee479a991d1111c03bd2321baf7ca9fa6065
sophavin <116633924+lysophavin18@users.noreply.github.com>
2023-03-27 03:59:33 +0000

    add dynamic data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -10,22 +10,25 @@ COURSES = [{
 }, {
     'id': 2,
     'name school': "អនុវិទ្យាល័យបន្លិចប្រាសាទ,Secondary Banlich Prasat ",
-    'Location':
-    "8M2+C6, អ្នកលឿង",
+    'Location': "8M2+C6, អ្នកលឿង",
     'Year': '2016-2018'
 }, {
     'id': 3,
     'name school': "វិទ្យាល័យពាមរក៍, Peam Ro High School",
-    'Location':
-    "875J+JHH, ផ្លូវជាតិលេខ ១១, ពាមរក៍",
+    'Location': "875J+JHH, ផ្លូវជាតិលេខ ១១, ពាមរក៍",
     'Year': '2019-2021'
-},
-          {
+}, {
     'id': 4,
-    'name school': "RUPP  Faculty Of Engineering Department Information engineering ",
+    'name school':
+    "RUPP  Faculty Of Engineering Department Information engineering ",
     'Location': "Russian Federation Boulevard, Toul Kork, Phnom Penh",
     'Year': '2021-so on'
 }]
+
+
+@app.route("/api/courses")
+def list_courses():
+    return jsonify(COURSES)
 
 
 @app.route("/")
